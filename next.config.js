494cd2508134ccc34890699ff7f03b63da53d145
unknown <debbiemeer30@gmail.com>
2025-03-05 02:28:41 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
